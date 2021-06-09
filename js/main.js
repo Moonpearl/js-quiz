3c9522f44b52add9a50bb42b81e0ff5436ce04d1
Romain Tello <romain.tello@gmail.com>
2021-06-09 12:30:55 +0200

    Replace static data with API requests

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 // Définit une action permettant d'afficher une question dans la page
 /**
- * Diplay a question in the DOM.
- * @param {number} index The index of the question to diplay.
+ * Display a question in the DOM.
+ * @param {object} question The question to display
  */
-function displayQuestion(index) {
+function displayQuestion(question) {
   // Récupère l'élément qui contient le numéro de la question
   const questionId = document.getElementById("question-id");
   // Modifie le contenu de cet élément
-  questionId.innerText = index + 1;
+  questionId.innerText = currentQuestionId;
 
   // Récupère l'élément qui contient le texte de la question
   const questionText = document.getElementById("current-question-text");
   // Remplace le contenu de cet élément par le texte de la question actuelle
-  questionText.innerText = questionData[index].text;
+  questionText.innerText = question.text;
+}
 
-  // Pour chaque numéro de réponse variant de 1 à 4
-  for (let i = 1; i <= 4; i++) {
+// Définit une action permettant d'afficher la liste des réponses la page
+/**
+ * Display answers in the DOM.
+ * @param {array} answers The list of answers to display.
+ */
+function displayAnswers(answers) {
+  let i = 1;
+  // Pour chaque réponse
+  for (let answer of answers['hydra:member']) {
     // Récupère l'élément correspondant au numéro de la réponse
     const answerCaption = document.getElementById("answer" + i + "-caption");
     // Remplace le contenu de cet élément par le texte de la réponse
     const key = "answer" + i;
-    answerCaption.innerText = questionData[index][key]
-  };
+    answerCaption.innerText = answer.text;
+    // Récupère le bouton radio associé à la réponse
+    const answerRadio = document.getElementById('answer' + i);
+    // Remplace la valeur retournée par le bouton par l'ID de la réponse
+    answerRadio.value = answer.id;
+    i++;
+  }
 }
 
 // Définit une fonction qui permet de créer un nouvel élément dans la liste de réponses
@@ -55,9 +68,7 @@ function createAnswersListItem(text, success) {
 function handleSubmit(event) {
   // Empêche le rechargement de la page
   event.preventDefault();
- 
-  // Récupère le numéro de la bonne réponse à la question actuelle
-  const rightAnswer = questionData[currentQuestionIndex].rightAnswer
+
   // Récupère le numéro de la réponse fournie par l'utilisateur
   const formData = $('#question-form').serializeArray();
   // Si le formulaire ne contient pas de donnée, affiche une alerte et interrompt la fonction
@@ -65,23 +76,90 @@ function handleSubmit(event) {
     alert('Vous devez sélectionner une réponse!');
     return;
   } 
-  const userAnswer = Number(formData[0].value);
   
-  // Récupère la liste des réponses dans la page
+  // Envoie une requête dans le serveur pour récupérer la bonne réponse à la question actuelle
+  fetch('http://localhost:8000/api/questions/' + currentQuestionId + '/right_answer')
+  // Associe une action au fait que le serveur réponde
+  .then(
+    // Transforme le contenu de la réponse en objet Javascript suivant le format JSON
+    function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+    }
+  )
+  // Associe une action au fait que la transformation en objet JSON soit terminée
+  .then(
+    // Vérifie si la réponse donnée était bonne ou non
+    function(rightAnswer) {
+      const userAnswer = Number(formData[0].value);
 
-  // Détermine si le numéro de la réponse fournie par l'utilisateur est égal au numéro de la bonne réponse à la question actuelle
-  const isUserAnswerRight = rightAnswer === userAnswer;
-  // Crée un nouvel élément dans la liste de réponses en tenant compte de s'il s'agit d'un succès ou d'un échec
-  createAnswersListItem(questionData[currentQuestionIndex].text, isUserAnswerRight);
+      // Détermine si le numéro de la réponse fournie par l'utilisateur est égal au numéro de la bonne réponse à la question actuelle
+      const isUserAnswerRight = rightAnswer.id === userAnswer;
 
-  // Augmente l'index de la question actuelle
-  currentQuestionIndex++;
-  // S'il n'y a plus de question, réinitialise l'index de la question actuelle
-  if (currentQuestionIndex === questionData.length) {
-    currentQuestionIndex = 0;
-  }
-  // Affiche la question actuelle dans la page
-  displayQuestion(currentQuestionIndex);
+      // Crée un nouvel élément dans la liste de réponses en tenant compte de s'il s'agit d'un succès ou d'un échec
+      createAnswersListItem(currentQuestion.text, isUserAnswerRight);
+
+      // Augmente l'index de la question actuelle
+      currentQuestionId++;
+
+      // Récupère et affiche la nouvelle question du serveur
+      fetchQuestion(currentQuestionId);
+    }
+  );
+  
+}
+
+let currentQuestion;
+/**
+ * Fetch and display a question from the API.
+ * @param {*} id The ID of the question to fetch.
+ */
+function fetchQuestion(id) {
+  // Envoie une requête sur un serveur distant permettant de récupérer la question
+  fetch('http://localhost:8000/api/questions/' + id)
+  // Associe une action au fait que le serveur réponde
+  .then(
+    // Transforme le contenu de la réponse en objet Javascript suivant le format JSON
+    function(response) {
+      if (response.ok) {
+        return response.json()
+      }
+
+      if (response.status === 404) {
+        currentQuestionId = 1;
+        fetchQuestion(currentQuestionId);
+      }
+    }
+  )
+  // Associe une action au fait que la transformation en objet JSON soit terminée
+  .then(
+    // Affiche la question dans la page
+    function(question) {
+      currentQuestion = question;
+      displayQuestion(question);
+    }
+  );
+
+  // Envoie une requête sur un serveur distant permettant de récupérer les réponses à la question
+  fetch('http://localhost:8000/api/questions/' + id + '/answers')
+  // Associe une action au fait que le serveur réponde
+  .then(
+    // Transforme le contenu de la réponse en objet Javascript suivant le format JSON
+    function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+    }
+  )
+  // Associe une action au fait que la transformation en objet JSON soit terminée
+  .then(
+    // Affiche les réponses à la question dans la page
+    function(answers) {
+      displayAnswers(answers);
+    }
+  );
+
 }
 
 // Récupère le formulaire
@@ -90,6 +168,6 @@ const form = document.getElementById("question-form");
 form.addEventListener("submit", handleSubmit);
 
 // Initialise l'index de la question actuelle
-let currentQuestionIndex = 0;
-// Affiche la première question
-displayQuestion(currentQuestionIndex);
+let currentQuestionId = 1;
+// Récupère et affiche la première question du serveur
+fetchQuestion(currentQuestionId);
